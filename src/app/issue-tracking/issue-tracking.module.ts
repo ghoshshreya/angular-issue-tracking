3c0e05361772fedb6156471655b354c59dcd9e3d
ghoshshreya <shrghosh08@gmail.com>
2023-06-17 00:37:23 +0530

    2 files modified, 1 added

--- src/app/issue-tracking/issue-tracking.module.ts
+++ src/app/issue-tracking/issue-tracking.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [DashboardComponent]
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [DashboardComponent],
 })
-export class IssueTrackingModule { }
+export class IssueTrackingModule {}
